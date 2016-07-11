2bac0f11f9f8e42d2586c11b3c7057f19a5e3d4b
Daniel Stratton <dstrat28@gmail.com>
2016-07-10 21:50:08 -0400

    Fixed git clone/checkout

--- modules/git/clone/clone.sh
+++ modules/git/clone/clone.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-echo "$ git clone --branch=$BRANCH $GIT_URL $FILE_PATH"
 echo -e "$GITHUB_SSH_KEY" > /id_rsa
 chmod 600 /id_rsa
 export GIT_SSH_COMMAND="ssh -i /id_rsa -o StrictHostKeyChecking=no"
-git clone --branch=$BRANCH $GIT_URL $FILE_PATH
+
+echo "$ git clone --branch=$BRANCH --depth=50 $GIT_URL $FILE_PATH"
+git clone --branch=$BRANCH --depth=50 $GIT_URL $FILE_PATH
+
+if [ -z ${PULL_ID+x} ];
+then
+    cd $FILE_PATH
+    echo "$ git checkout $COMMIT_ID"
+    git checkout $COMMIT_ID
+else
+    echo "$ cd $FILE_PATH"
+    cd $FILE_PATH
+    echo "$ git fetch origin pull/$PULL_ID/head:$PULL_ID"
+    git fetch origin pull/$PULL_ID/head:$PULL_ID
+    echo "$ git checkout $PULL_ID"
+    git checkout $PULL_ID
+fi
+
 rm -rf /id_rsa
+
 if [ $? -eq 0 ]
 then
     echo "git clone exited with 0"
