e06e31df70125aae8d9f3629f005d7de4fa5578e
Divya Karuturi <dkarutu@us.ibm.com>
2016-07-13 11:51:19 -0400

    fixed script

--- modules/license-checker/lc.sh
+++ modules/license-checker/lc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-license-checker
+echo "$ node -v"
+node -v
+echo "$ cd $FILE_PATH"
+cd $FILE_PATH
+echo "$FILE_PATH"
+echo "$ license-checker --json"
+license-checker --json
 ##cd into volume path
 ##switch to json
 ##more useful output
