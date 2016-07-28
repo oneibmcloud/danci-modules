0e23d605338ea941f65926ea34afe0f072b730e7
Daniel Stratton <dstrat28@gmail.com>
2016-07-28 11:15:17 -0400

    Updated chart format

--- modules/node-licenses/parser.js
+++ modules/node-licenses/parser.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec;
 var child;
 var array = [];
-var command = "license-checker";
+var command = 'license-checker';
 var util = require('util');
 
 var aContainsB = function(a, b) {
@@ -10,21 +10,25 @@ var aContainsB = function(a, b) {
 
 //reading input of license-scanner
 child = exec(command, function(error, stdout, stderr) {
+    if (error) {
+        console.log('exec error: ' + error);
+    }
+
     var lines = stdout.split('\n');
 
     //iterate through each line of output
     for (var i = 0; i < lines.length; i++) {
 
         //check if the current line is lists the license
-        if (aContainsB(lines[i], "licenses: ")) {
-            var tempKey = lines[i].split("licenses: ")[1];
+        if (aContainsB(lines[i], 'licenses: ')) {
+            var tempKey = lines[i].split('licenses: ')[1];
             //make a new object that contains the license object
 
             var check = true;
 
             //iterate through the array with license objects
             for (var j = 0; j < array.length; j++) {
-                //console.log("Object.keys(array[j]): "+Object.keys(array[j]));
+                //console.log('Object.keys(array[j]): '+Object.keys(array[j]));
                 //if it already has this object, update count
                 if (aContainsB(array[j][0], tempKey)) {
                     array[j][1]++;
@@ -37,7 +41,6 @@ child = exec(command, function(error, stdout, stderr) {
                 tempObject.push(tempKey);
                 tempObject.push(1);
                 array.push(tempObject);
-
             }
         }
     }
@@ -48,54 +51,38 @@ child = exec(command, function(error, stdout, stderr) {
     });
 
     //initialize data for chart
-    chart_array = [];
-    var licenses = ['License'];
-    var count = ['License Info'];
+    var licenses = [];
+    var count = [];
 
     //build string for summary and fill data for chart
-    var datastring = "DANCI_STEP_SUMMARY_";
+    var datastring;
     for (var m = 0; m < array.length - 1; m++) {
-        datastring += array[m][0] + " (" + array[m][1] + "), ";
-        licenses.push(array[m][0]);
-        licenses.push({role: 'annotation'});
+        datastring += array[m][0] + ' (' + array[m][1] + '), ';
+        licenses.push(array[m][0] + ' (' + array[m][1] + ')');
         count.push(array[m][1]);
-        count.push(array[m][1].toString());
-
     }
 
     //final addition to datastring
-    datastring += array[array.length - 1][0] + " (" + array[array.length - 1][1] + ")";
+    datastring += array[array.length - 1][0] + ' (' + array[array.length - 1][1] + ')';
+    licenses.push(array[array.length - 1][0] + ' (' + array[array.length - 1][1] + ')');
+    count.push(array[array.length - 1][1]);
 
-    //push licenses and count to final data array
-    chart_array.push(licenses);
-    chart_array.push(count);
+    var chart_data = {
+        labels: licenses,
+        datasets: [
+            {
+                data: count
+            }
+        ]
+    };
 
     var data = {
-        'type': "BarChart",
+        'type': 'doughnut',
         'latest': true,
-        'data': chart_array,
+        'data': chart_data,
         'title': 'Node.js License Info',
-        'options': {
-            isStacked: true,
-            hAxis: {
-                baselineColor: 'transparent',
-                textPosition: 'none',
-                gridlines: {
-                    color: 'transparent'
-                }
-            },
-            vAxis: {
-                baselineColor: 'transparent',
-                textPosition: 'none'
-            },
-            backgroundColor: {
-                fill: 'transparent'
-            },
-            legend: {
-                position: 'none'
-            },
-            width: 750,
-            height: 300
+        'animation': {
+            animateScale: true
         }
     };
 
@@ -103,15 +90,11 @@ child = exec(command, function(error, stdout, stderr) {
 
     //print to show ouput to console
     console.log(array);
-    //
-    // //print to store in module database
-    console.log("DANCI_MODULE_DATA_" + JSON.stringify(data));
-    //
-    // //print summary string
-    console.log(datastring);
-    console.log('DANCI_STEP_STATUS_SUCCESS');
 
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
+    // //print to store in module database
+    console.log('DANCI_MODULE_DATA_' + JSON.stringify(data));
+
+    // //print summary string
+    console.log('DANCI_STEP_SUMMARY_' + datastring);
+    console.log('DANCI_STEP_STATUS_SUCCESS');
 });
