bdafac09ea5b14f5ba9e0c180fbdacb99d017130
Daniel Stratton <dstrat28@gmail.com>
2016-08-02 10:20:48 -0400

    Updated jenkins module

--- modules/jenkins/main.js
+++ modules/jenkins/main.js
@@ -1,1 +1,30 @@
 var jenkins = require('jenkins')(process.env.JENKINS_URL);
+
+//Trigger new build
+jenkins.job.build('example', function(err) {
+    if (err)
+        return err;
+    }
+);
+
+//Get build status
+function buildStatus(buildName, buildId) {
+    jenkins.build.get(buildName, buildId, function(err, data) {
+        if (err)
+            return err;
+        if (data.inQueue === false) {
+            buildLog(buildName, buildId);
+        } else {
+            buildStatus(buildName, buildId);
+        }
+    });
+}
+
+//Get build log
+function buildLog(buildName, buildId) {
+    jenkins.build.get(buildName, buildId, function(err, data) {
+        if (err)
+            return err;
+        console.log(data);
+    });
+}
