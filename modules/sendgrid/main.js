143f415142292e1afa1f21ca620b5064825f368c
Divya Karuturi <dkarutu@us.ibm.com>
2016-08-03 09:57:07 -0400

    fixed sendgrid structure, plz try it

--- modules/sendgrid/main.js
+++ modules/sendgrid/main.js
@@ -8,5 +8,9 @@ sendgrid.send({
     subject: 'EMAIL_SUBJECT',
     text: 'EMAIL_BODY'
 }, function(err) {
-    console.log("successful: " + err);
+  if (err) {
+     console.log('DANCI_ERROR: running git clone');
+     console.log('DANCI_STEP_SUMMARY_Error running git clone');
+     return console.log('DANCI_STEP_STATUS_FAILURE');
+ }
 });
