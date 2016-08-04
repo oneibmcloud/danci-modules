761a9feb443a5f18512c4db9dd2f7e3735fdb5c8
Daniel Stratton <dstrat28@gmail.com>
2016-08-04 12:28:33 -0400

    Updated module error checking

--- modules/deploy-cf/main.sh
+++ modules/deploy-cf/main.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+echo "cf api $DEPLOY_API"
 /script/cf api $DEPLOY_API
+
+if [ $? -eq 0 ]
+then
+    echo "cf api $DEPLOY_API exited with 0"
+else
+    echo "DANCI_ERROR_Error running cf api $DEPLOY_API" >&2
+    echo "DANCI_STEP_SUMMARY_Error running cf api $DEPLOY_API"
+    echo "DANCI_STEP_STATUS_FAILURE"
+    exit
+fi
+
+echo "cf login -u $DEPLOY_USERNAME -p [PRIVATE] -o $DEPLOY_ORGANIZATION -s $DEPLOY_SPACE"
 /script/cf login -u $DEPLOY_USERNAME -p $DEPLOY_PASSWORD -o $DEPLOY_ORGANIZATION -s $DEPLOY_SPACE
+
+if [ $? -eq 0 ]
+then
+    echo "cf login -u $DEPLOY_USERNAME -p [PRIVATE] -o $DEPLOY_ORGANIZATION -s $DEPLOY_SPACE exited with 0"
+else
+    echo "DANCI_ERROR_Error running cf login -u $DEPLOY_USERNAME -p [PRIVATE] -o $DEPLOY_ORGANIZATION -s $DEPLOY_SPACE" >&2
+    echo "DANCI_STEP_SUMMARY_Error running cf login -u $DEPLOY_USERNAME -p [PRIVATE] -o $DEPLOY_ORGANIZATION -s $DEPLOY_SPACE"
+    echo "DANCI_STEP_STATUS_FAILURE"
+    exit
+fi
+
+echo "cf push $APP_NAME -p $FILE_PATH"
 /script/cf push $APP_NAME -p $FILE_PATH
+
+if [ $? -eq 0 ]
+then
+    echo "cf push $APP_NAME -p $FILE_PATH exited with 0"
+    echo "DANCI_STEP_SUMMARY_Successfully Deployed $APP_NAME"
+    echo "DANCI_STEP_STATUS_SUCCESS"
+else
+    echo "DANCI_ERROR_Error running cf push $APP_NAME -p $FILE_PATH" >&2
+    echo "DANCI_STEP_SUMMARY_Error running cf push $APP_NAME -p $FILE_PATH"
+    echo "DANCI_STEP_STATUS_FAILURE"
+    exit
+fi
