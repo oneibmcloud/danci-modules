59016c8dfcccaaed94a08c96a5b39368561f0569
Divya Karuturi <dkarutu@us.ibm.com>
2016-08-04 17:18:28 -0400

    error message fixed

--- modules/slack-bot/main.js
+++ modules/slack-bot/main.js
@@ -11,8 +11,6 @@ slack.webhook({
   text: "Here is a link to the build history: <"+ process.env.DANCI_BUILD_URL+">"
 }, function(err, response) {
   if (err) {
-          console.log('DANCI_ERROR: running git clone');
-          console.log('DANCI_STEP_SUMMARY_Error running git clone');
-          return console.log('DANCI_STEP_STATUS_FAILURE');
+          return console.log('slack post failed');
       }
 });
